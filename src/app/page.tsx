ae7cd0e137897b858126284d6cf2585db1d4cb98
miki <miki@wsl.com>
2025-02-06 14:17:08 +0100

    for the moon

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <>
+      <Button>sieam</Button>
+      <h1 className="m-2 p-5 font-bold text-rose-300">gells</h1>
+      <div>kurcze</div>
+    </>
+  );
+}
