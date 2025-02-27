a7bc1d4cb243bba58c820ecf53f012dad71464b8
miki <mikoz7388@gmail.com>
2025-02-27 12:19:46 +0100

    fix: enable dynamic rendering for studio page

--- src/app/(studio)/studio/page.tsx
+++ src/app/(studio)/studio/page.tsx
@@ -2,6 +2,8 @@ import { DEFAULT_INFINITE_QUERY_LIMIT } from "@/lib/constants";
 import { StudioView } from "@/modules/studio/ui/view/studio-view";
 import { HydrateClient, trpc } from "@/trpc/server";
 
+export const dynamic = "force-dynamic";
+
 export default async function Page() {
   void trpc.studio.getMany.prefetchInfinite({
     limit: DEFAULT_INFINITE_QUERY_LIMIT,
